1b7f36bcd91562c33b756187558505349a1b2da6
Syed Haris <syedharishassan31@gmail.com>
2024-07-21 23:47:18 +0500

    Add details in index file

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+
+//Dot ENC Config
 dotenv.config();
+
+//mongodb Connection
+
+//Rest Object
+const app = express();
+
+//routes
+app.get("/", (req, resp) => {
+  resp.send("<h1> Welcome to Job Portal Application <h1/>");
+});
+
+//Port
+const PORT = process.env.PORT || 4000;
+
+//Listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DED_MODE} Mode on Port no ${PORT}`.bgCyan
+  );
+});
+//npm run index
